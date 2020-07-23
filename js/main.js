158c00502affb8c72fe0240d188560b0c16fb0b3
John Hu <johnhtj@gmail.com>
2020-07-22 19:08:56 -0700

    check-in pages and code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+var totalCheckIns = 0;
 
+function toggleCheckIn() {
+    var checkIn = document.getElementById("check-in");
+    if (checkIn.innerHTML == "Remove Check In") { //if the user cancels the check in
+        localStorage.setItem ("checkInState", "");
+        totalCheckIns--;
+        checkIn.innerHTML = "Check In";
+        document.getElementById("check-ins").innerHTML = totalCheckIns;
+    } else { //if the user checks in
+        localStorage.setItem ("checkInState", "true"); //supposed to store the check-in state to cache
+        totalCheckIns++;
+        checkIn.innerHTML = "Remove Check In";
+        document.getElementById("check-ins").innerHTML = totalCheckIns;
+    }
+}
+
+function showCheckIns() {
+    var checkInStatus = localStorage.getItem("checkInState");
+    var checkInList = document.getElementById("check-in-item");
+    if (checkInStatus = "true" ) { // if checked in, it should display the list
+        checkInList.style.display = "block";
+    }else {
+        checkInList.style.display = "none";
+    }
+}
